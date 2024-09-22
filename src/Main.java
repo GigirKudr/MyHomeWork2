1d64da696d458455090f1df0ffb6d5eaf98788ea
Anton Kudryashov <driver_90@list.ru>
2024-09-22 19:42:51 +0700

    first commit

---
+++ src/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) {
+        int newTicketPrice = 13_676;
+        int bonusBall = 20;
+        int resultBonus = newTicketPrice / bonusBall;
+        System.out.println(resultBonus);
+    }
+}
